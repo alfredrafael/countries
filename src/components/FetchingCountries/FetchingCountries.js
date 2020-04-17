66f4a7c1f0c6f2a5fb797f023e4965c3392a04dc
alfredrafael <alfredrafael@gmail.com>
2020-04-17 19:46:46 -0400

    Redirects with name of clicked coutnry

--- src/components/FetchingCountries/FetchingCountries.js
+++ src/components/FetchingCountries/FetchingCountries.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, Component} from 'react';
 import { Link } from '@reach/router';
 import {API_URL_ALL} from '../../config';
 import { Container } from 'reactstrap';
@@ -46,7 +46,7 @@ return(
     {
     stateFromAPI.map((country, index) => (
         <div key={index} style={{marginBottom: '7%'}}>
-            <Link to={`/${country.name}`}>
+            <Link to={`/${country.name}`} onClick={e=>this.context.router.transitionTo(`/${country.name}`)}>
                 <p>{country.name}</p>
                 <img style={{maxWidth: '100px', border: '1px solid grey'}} src={country.flag}/>
             </Link>     
